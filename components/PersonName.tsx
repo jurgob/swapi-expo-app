68358cffecf1a85270b07ef4dc9c0612054b2e8a
jurgob <jurgob@users.noreply.github.com>
2024-12-09 01:33:00 +0000

    add link + deploy

--- components/PersonName.tsx
+++ components/PersonName.tsx
@@ -1,6 +1,7 @@
 import { ThemedText } from './ThemedText';
 import { utils } from '@/clients/starwars';
 import { useStarWarsGetPerson, useStarWarsGetPlanet } from '@/hooks/starwarsapi';
+import { Link } from 'expo-router';
 const { urlToPersonId } = utils;
 
 export function PersonName({url}: {url:string}) {
@@ -12,6 +13,6 @@ export function PersonName({url}: {url:string}) {
     }
 
     return (
-        <ThemedText >{data.name}</ThemedText>
+        <Link href={`/people/${personId}`} ><ThemedText >{data.name}</ThemedText></Link>
     );
   }
